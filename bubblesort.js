5ac2355c5ea80ebbd1621f11e461494b02cee623
angel-chen <rikugou@gmail.com>
2018-04-23 13:50:54 -0400

    Added swapped true/false

--- bubblesort.js
+++ bubblesort.js
@@ -3,20 +3,31 @@
 
 
 function bubbleSort(array) {
-  for(let i = 0; i < array.length; i++) {
-    compare(array, i, i + 1)
+  let swapped = false;
+  for(let i = 0; i < array.length-1; i++) {
+    debugger;
+    if(!swapped) {
+      compare(array, i, i + 1)
+    }
+    else {
+      continue;
+    }
     }
     return array;
   }
 
 function compare(array, x, y) {
   if (array[x] > array[y]) {
-    swap(array, array[x], array[y])
+    swap(array, array[x], array[y]);
+    swapped = true;
+  }
+  else {
+    swapped = true;
   }
 }
 
 function swap(array, x, y) {
   let temp = array[x];
-  array[y] = temp;
   array[x] = array[y];
+  array[y] = temp;
 }
