412c0877157e3b743cf580c3669e2c3183468f33
Joyce Chow <joycecyoj01@gmail.com>
2018-04-23 12:03:47 -0400

    wrote test specs

---
+++ bubble.spec.js
@@ -0,0 +1,32 @@
+describe('Bubble Sort', function(){
+  let unsortedArr = [15, 25, 5, 17, 1];
+  let sortedArr = [1, 5, 15, 17, 25];
+
+  beforeAll(function () {
+    spyOn( 'swap').and.callThrough();
+    spyOn( 'compare').and.callThrough();
+  });
+
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('bubble sorts an array', function(){
+    expect( bubbleSort(unsortedArr)). toEqual(sortedArr);
+  });
+
+  it('returns arr if only one item long', function(){
+    expect( bubbleSort([1])). toEqual([1]);
+  });
+
+  it('number of swaps taken is correct', function() {
+    bubbleSort(unsortedArr);
+    expect(swap.calls.count()).toEqual(8);
+  });
+
+  it('number of comparisons taken is correct', function() {
+    bubbleSort(unsortedArr);
+    expect(compare.calls.count()).toEqual(14);
+  });
+
+});
---
+++ bubblesort.js
@@ -0,0 +1,18 @@
+// Notes:
+// keep track of how many comparisons and swaps performed for each sort
+
+
+function bubbleSort(array) {
+
+
+
+
+}
+
+function compare(x, y) {
+  
+}
+
+function swap(x, y) {
+
+}
